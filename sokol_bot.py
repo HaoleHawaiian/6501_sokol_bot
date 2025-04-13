6b5ecc77dd2c64164855cf92a34cb59e74b3891f
HaoleHawaiian <alohaethan@yahoo.com>
2025-04-13 13:27:31 -0700

    Corrected name on model for math and code

--- sokol_bot.py
+++ sokol_bot.py
@@ -9,7 +9,7 @@ import streamlit as st
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 # Load model and tokenizer
-model_name = "TinyLlama/TinyLlama_v1.1_MathAndCode"  # or "TinyLlama/TinyLlama_v1.1" for standard
+model_name = "TinyLlama/TinyLlama_v1.1_math_code"  # or "TinyLlama/TinyLlama_v1.1" for standard
 model = AutoModelForCausalLM.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
